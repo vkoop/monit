5b5f5190b8d9fb994560e1a668334d67a4eb7dbb
Viktor Koop <viktor.koop@googlemail.com>
2017-12-01 18:35:52 +0100

    use lombok

--- src/main/java/de/vkoop/monit/config/MyConfigProperties.java
+++ src/main/java/de/vkoop/monit/config/MyConfigProperties.java
@@ -1,38 +1,20 @@
 package de.vkoop.monit.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+@Data
 @ConfigurationProperties
 public class MyConfigProperties {
+
     public List<MyConfigEntry> myhosts;
 
-    public void setMyhosts(List<MyConfigEntry> myhosts) {
-        this.myhosts = myhosts;
-    }
-
-    public List<MyConfigEntry> getMyhosts() {
-        return myhosts;
-    }
-
+    @Data
     public static class MyConfigEntry {
-
         public int port;
         public String ip;
         public String name;
-
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setPort(int port) {
-            this.port = port;
-        }
-
-        public void setIp(String ip) {
-            this.ip = ip;
-        }
     }
 }
