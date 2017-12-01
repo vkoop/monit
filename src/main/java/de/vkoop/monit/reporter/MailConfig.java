7a8f55d1bd38fdebcb39a9cdd94d185d29d76f87
Viktor Koop <viktor.koop@googlemail.com>
2017-12-01 18:51:10 +0100

    use lombok

--- src/main/java/de/vkoop/monit/reporter/MailConfig.java
+++ src/main/java/de/vkoop/monit/reporter/MailConfig.java
@@ -1,29 +1,15 @@
 package de.vkoop.monit.reporter;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
 @EnableConfigurationProperties
 @ConfigurationProperties("mailconfig")
 public class MailConfig {
     String fromMail;
     public String toEmail;
-
-    public String getFromMail() {
-        return fromMail;
-    }
-
-    public void setFromMail(String fromMail) {
-        this.fromMail = fromMail;
-    }
-
-    public String getToEmail() {
-        return toEmail;
-    }
-
-    public void setToEmail(String toEmail) {
-        this.toEmail = toEmail;
-    }
 }
