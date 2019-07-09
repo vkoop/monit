30bd1759b1601290414b1230fc5cca9d4595cb78
Viktor Koop <viktor.koop@googlemail.com>
2019-07-09 18:47:41 +0200

    invalid https

--- src/main/java/de/vkoop/monit/config/HttpClientConfiguration.java
+++ src/main/java/de/vkoop/monit/config/HttpClientConfiguration.java
@@ -1,6 +1,7 @@
 package de.vkoop.monit.config;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ public class HttpClientConfiguration {
 
     @Bean
     public HttpClient getHttpClient() {
-        return HttpClients.createDefault();
+        return HttpClients.custom()
+                .setSSLHostnameVerifier(AllowAllHostnameVerifier.INSTANCE)
+                .build();
     }
 }
