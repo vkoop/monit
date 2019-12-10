01a3964276c647fae2dafef77189ef591c0e6c53
Viktor Koop <viktor.koop@googlemail.com>
2019-12-10 13:59:04 +0100

    update gradle and java

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ group = "de.vkoop"
 version = "0.0.5-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	targetCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks.withType<JavaCompile> {
